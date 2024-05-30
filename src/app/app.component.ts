6dafdde7fc9b391031312b87e9f21fb5f9783ee0
Chau Tran <nartc7789@gmail.com>
2024-05-29 21:06:32 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet],
+	template: `
+		<h1>Welcome to {{ title }}!</h1>
 
-    <router-outlet />
-  `,
-  styles: [],
+		<router-outlet />
+	`,
+	styles: [],
 })
 export class AppComponent {
-  title = 'ngt-ping-pong';
+	title = 'ngt-ping-pong';
 }
